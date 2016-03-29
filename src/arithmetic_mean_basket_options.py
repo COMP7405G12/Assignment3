d01368befcb5c4dc161d09b7d18020d40890360e
WarnWang <wangyouan0629@hotmail.com>
2016-03-29 19:53:10 +0800

    Finish arithmetic mean basket option calculator web page

--- src/arithmetic_mean_basket_options.py
+++ src/arithmetic_mean_basket_options.py
@@ -8,10 +8,10 @@
 
 import math
 
-import web
 import numpy as np
-from scipy.stats import norm
 import numpy.random as random
+import web
+from scipy.stats import norm
 
 from __init__ import render
 
@@ -37,9 +37,9 @@ class Option(object):
     Class to represent a option
     '''
 
-    def __init__(self, s0, strike_price, sigma, type, tau, risk_free_rate):
+    def __init__(self, s0, strike_price, sigma, option_type, tau, risk_free_rate):
         self.sigma = float(sigma)
-        self.type = type
+        self.type = option_type
         self.strike_price = float(strike_price)
         self.s0 = float(s0)
         self.tau = float(tau)
@@ -56,13 +56,13 @@ class Option(object):
 
 
 class BasketOptions(object):
-    def __init__(self, s10, s20, k, sigma1, sigma2, type, tau, rho, risk_free_rate=RISK_FREE_RATE):
-        self.option1 = Option(s10, k, sigma1, type, tau, risk_free_rate)
-        self.option2 = Option(s20, k, sigma2, type, tau, risk_free_rate)
+    def __init__(self, s10, s20, k, sigma1, sigma2, option_type, tau, rho, risk_free_rate=RISK_FREE_RATE):
+        self.option1 = Option(s10, k, sigma1, option_type, tau, risk_free_rate)
+        self.option2 = Option(s20, k, sigma2, option_type, tau, risk_free_rate)
         self.B0 = float(s10 + s20) / 2
         self.tau = float(tau)
         self.strike_price = float(k)
-        self.type = type
+        self.type = option_type
         self.risk_free_rate = risk_free_rate
         self.rho = rho
 
@@ -113,6 +113,10 @@ class BasketOptions(object):
     def get_basket_price_with_control_variate(self, path_number):
         arithmetic_price, s1, s2 = self._get_basket_price(path_number)
         geo_mean_price = self._get_geometric_price()
+        arith_mean = arithmetic_price.mean()
+        arith_std = arithmetic_price.std()
+        value = [arith_mean, arith_mean - 1.96 * arith_std / math.sqrt(path_number),
+                 arith_mean + 1.96 * arith_std / math.sqrt(path_number)]
 
         # geometric price
         basket_geo_price = np.sqrt(s1 * s2)
@@ -132,8 +136,9 @@ class BasketOptions(object):
         control_variate = arithmetic_price + theta * (geo_mean_price - geometric_price)
         control_mean = control_variate.mean()
         control_std = control_variate.std()
-        return control_mean, control_mean - 1.96 * control_std / math.sqrt(path_number), \
-               control_mean + 1.96 * control_std / math.sqrt(path_number)
+        value.extend([control_mean, control_mean - 1.96 * control_std / math.sqrt(path_number),
+                      control_mean + 1.96 * control_std / math.sqrt(path_number)])
+        return value
 
     def get_basket_price(self, path_number=PATH_NUMBER, has_control=False):
         if has_control:
@@ -142,7 +147,7 @@ class BasketOptions(object):
             maturity_price = self._get_basket_price(path_number)[0]
             mean = maturity_price.mean()
             std = maturity_price.std()
-            return mean, mean - 1.96 * std / math.sqrt(path_number), mean + 1.96 * std / math.sqrt(path_number)
+            return [mean, mean - 1.96 * std / math.sqrt(path_number), mean + 1.96 * std / math.sqrt(path_number)]
 
 
 class ArithmeticMeanBasketOptionsHTML(object):
@@ -150,12 +155,36 @@ class ArithmeticMeanBasketOptionsHTML(object):
         return render.arithmetic_mean_basket_options()
 
     def POST(self):
-        return render.arithmetic_mean_basket_options()
+        data = web.input()
+        try:
+            stock1 = float(data['stock1'])
+            stock2 = float(data['stock2'])
+            vol1 = float(data['vol1'])
+            vol2 = float(data['vol2'])
+            strike = float(data['strike'])
+            maturity = float(data['time'])
+            rate = float(data['rate']) / 100
+            corr = float(data['corr'])
+            num = int(data['num'])
+            option_type = data['type']
+            cv_type = data['cv_type']
+        except ValueError, e:
+            return render.arithmetic_mean_basket_options(price="Invalid input, please input again")
+        else:
+            calculator = BasketOptions(s10=stock1, s20=stock2, k=strike, sigma2=vol2, sigma1=vol1, rho=corr,
+                                       option_type=option_type, tau=maturity, risk_free_rate=rate)
+            if cv_type == 'GBO':
+                price_list = list(calculator.get_basket_price(num, True))
+            else:
+                price_list = list(calculator.get_basket_price(num))
+            return render.arithmetic_mean_basket_options(stock1=stock1, vol1=vol1, stock2=stock2, vol2=vol2,
+                                                         strike=strike, corr=corr, rate=rate * 100, time=maturity,
+                                                         num=num, type=option_type, cv=cv_type, price=price_list)
 
 
 if __name__ == "__main__":
     test = BasketOptions(s10=100, s20=100, k=100, sigma1=0.3, sigma2=0.3, rho=0.5,
-                         type=PUT_OPTION, tau=MATURITY_TIME, risk_free_rate=RISK_FREE_RATE)
+                         option_type=PUT_OPTION, tau=MATURITY_TIME, risk_free_rate=RISK_FREE_RATE)
     print test.get_basket_price(path_number=PATH_NUMBER)
     print test.get_basket_price(has_control=True)
     # test = ArithmeticMeanBasketOptions(10, 10, 9, 0.1, 0.1, CALL_OPTION, 1)
