735fd5732780c7c333eb1ff76b6510606c1e212d
Mark Wang <wangyouan0629@hotmail.com>
2016-03-29 20:56:22 +0800

    Fix a small bug add index

--- src/index.py
+++ src/index.py
@@ -8,14 +8,21 @@
 
 import web
 
+from __init__ import render
 from eu_black_scholes import EuropeanOptionHtml
 from arithmetic_mean_basket_options import ArithmeticMeanBasketOptionsHTML
+
+
+class index(object):
+    def GET(self):
+        return render.index()
 
 
 if __name__ == "__main__":
     urls = (
         '/am_bo', ArithmeticMeanBasketOptionsHTML,
-        '/eu_bs', EuropeanOptionHtml
+        '/eu_bs', EuropeanOptionHtml,
+        '/', index
     )
     app = web.application(urls, globals())
     app.run()
