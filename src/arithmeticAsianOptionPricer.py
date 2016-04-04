50314d7e31a5fb313671eedc740734d478d70b6c
Warn <wangyouan0629@hotmail.com>
2016-04-04 20:02:16 +0800

    Try to optimize the code

--- src/arithmeticAsianOptionPricer.py
+++ src/arithmeticAsianOptionPricer.py
@@ -24,11 +24,8 @@ class arithmeticOption:
         self.method = method
 
     def arithmeticOptPricer(self):
-        # calculate geo Asian exact mean
-        sigsqT, muT, d1, d2, N1, N2, geo, drift = float(), float(), float(), float(), float(), float(), float(), float()
-        arithPayoff = list()
-        geoPayoff = list()
 
+        # calculate geo Asian exact mean
         sigsqT = self.sigma * self.sigma * self.T * (self.N + 1.0) * (2.0 * self.N + 1) / (6.0 * self.N * self.N)
         muT = 0.5 * sigsqT + (self.r - 0.5 * self.sigma * self.sigma) * self.T * (self.N + 1.0) / (2.0 * self.N)
 
@@ -43,36 +40,29 @@ class arithmeticOption:
             geo = max(math.exp(-self.r * self.T) * (self.E * (1 - N2) - self.S * math.exp(muT) * (1 - N1)), 0)
 
         drift = math.exp((self.r - 0.5 * self.sigma * self.sigma) * self.Dt)
-        random_value = norm.rvs(loc=0, scale=1, size=self.M)
-        for i in range(self.M):
-            growthFactor = drift * math.exp(self.sigma * math.sqrt(self.Dt) * random_value[i])
-            Spath = []
-            Spath.append(self.S * growthFactor)
-            n_rangdom_value = norm.rvs(loc=0, scale=1, size=self.N)
-            for j in range(1, self.N):
-                growthFactor = drift * math.exp(self.sigma * math.sqrt(self.Dt) * n_rangdom_value[j - 1])
-                Spath.append(Spath[j - 1] * growthFactor)
+        random_value = norm.rvs(loc=0, scale=1, size=(self.N, self.M))
+        Spath = numpy.exp(random_value * self.sigma * math.sqrt(self.Dt)) * drift
+        Spath[0] *= self.S
+        for i in range(1, self.N):
+            Spath[i] *= Spath[i - 1]
 
-            Spath = numpy.array(Spath)
+        arithMean = Spath.mean(0)
+        geoMean = numpy.exp(numpy.log(Spath).mean(0))
 
-            # Arithmetic mean
-            arithMean = Spath.mean()
+        if self.type == 'Put':
+            arithMean = self.E - arithMean
+            geoMean = self.E - geoMean
+        else:
+            arithMean -= self.E
+            geoMean -= self.E
 
-            if self.type == "Call":
-                arithPayoff.append(math.exp(-self.r * self.T) * max(arithMean - self.E, 0))
-            else:
-                arithPayoff.append(math.exp(-self.r * self.T) * max(self.E - arithMean, 0))
+        for i in range(len(arithMean)):
+            arithMean[i] = max(arithMean[i], 0)
+            geoMean[i] = max(geoMean[i], 0)
 
-            # geometic mean
-            geoMean = numpy.exp((numpy.log(Spath).mean()))
+        arithPayoff = math.exp(-self.r * self.T) * arithMean
+        geoPayoff = math.exp(-self.r * self.T) * geoMean
 
-            if self.type == "Call":
-                geoPayoff.append(numpy.exp(-self.r * self.T) * max(geoMean - self.E, 0))
-            if self.type == "Put":
-                geoPayoff.append(numpy.exp(-self.r * self.T) * max(self.E - geoMean, 0))
-
-        arithPayoff = numpy.array(arithPayoff)
-        geoPayoff = numpy.array(geoPayoff)
         if self.method == 0:
             # standard Monte Carlo
             Pmean = arithPayoff.mean()
@@ -84,9 +74,7 @@ class arithmeticOption:
             # control variate
             covXY = numpy.cov([arithPayoff, geoPayoff], ddof=0)[0][1]
             theta = float(covXY) / geoPayoff.var()
-            Z = list()
             Z = arithPayoff + theta * (geo - geoPayoff)
-            print geo
             Zmean = Z.mean()
             Zstd = Z.std()
             confcv = (Zmean - 1.96 * Zstd / math.sqrt(self.M), Zmean + 1.96 * Zstd / math.sqrt(self.M))
@@ -96,43 +84,43 @@ class arithmeticOption:
 
 if __name__ == '__main__':
     # testcase 01
-    aopricer = arithmeticOption(100.0, 100.0, 0.3, 0.05, 3, 50, "Put", 10000, 0)
+    aopricer = arithmeticOption(100.0, 100.0, 0.3, 0.05, 3, 50, "Put", 100000, 0)
     confmc0 = aopricer.arithmeticOptPricer()
-    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 50, "Put", 10000, 1)
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 50, "Put", 100000, 1)
     confmc1 = aopricer.arithmeticOptPricer()
     print (confmc0, confmc1)
 
-    # # testcase 02
-    # aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Put", 100000, 0)
-    # confmc2 = aopricer.arithmeticOptPricer()
-    # aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Put", 100000, 1)
-    # confmc3 = aopricer.arithmeticOptPricer()
-    # print (confmc2, confmc3)
-    #
-    # # testcase 03
-    # aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Put", 100000, 0)
-    # confmc4 = aopricer.arithmeticOptPricer()
-    # aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Put", 100000, 1)
-    # confmc5 = aopricer.arithmeticOptPricer()
-    # print (confmc4, confmc5)
-    #
-    # # testcase 04
-    # aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 50, "Call", 100000, 0)
-    # confmc6 = aopricer.arithmeticOptPricer()
-    # aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 50, "Call", 100000, 1)
-    # confmc7 = aopricer.arithmeticOptPricer()
-    # print (confmc6, confmc7)
-    #
-    # # testcase 05
-    # aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Call", 100000, 0)
-    # confmc8 = aopricer.arithmeticOptPricer()
-    # aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Call", 100000, 1)
-    # confmc9 = aopricer.arithmeticOptPricer()
-    # print (confmc8, confmc9)
-    #
-    # # testcase 06
-    # aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Call", 100000, 0)
-    # confmc10 = aopricer.arithmeticOptPricer()
-    # aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Call", 100000, 1)
-    # confmc11 = aopricer.arithmeticOptPricer()
-    # print (confmc10, confmc11)
+    # testcase 02
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Put", 100000, 0)
+    confmc2 = aopricer.arithmeticOptPricer()
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Put", 100000, 1)
+    confmc3 = aopricer.arithmeticOptPricer()
+    print (confmc2, confmc3)
+
+    # testcase 03
+    aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Put", 100000, 0)
+    confmc4 = aopricer.arithmeticOptPricer()
+    aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Put", 100000, 1)
+    confmc5 = aopricer.arithmeticOptPricer()
+    print (confmc4, confmc5)
+
+    # testcase 04
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 50, "Call", 100000, 0)
+    confmc6 = aopricer.arithmeticOptPricer()
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 50, "Call", 100000, 1)
+    confmc7 = aopricer.arithmeticOptPricer()
+    print (confmc6, confmc7)
+
+    # testcase 05
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Call", 100000, 0)
+    confmc8 = aopricer.arithmeticOptPricer()
+    aopricer.__init__(100.0, 100.0, 0.3, 0.05, 3, 100, "Call", 100000, 1)
+    confmc9 = aopricer.arithmeticOptPricer()
+    print (confmc8, confmc9)
+
+    # testcase 06
+    aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Call", 100000, 0)
+    confmc10 = aopricer.arithmeticOptPricer()
+    aopricer.__init__(100.0, 100.0, 0.4, 0.05, 3, 50, "Call", 100000, 1)
+    confmc11 = aopricer.arithmeticOptPricer()
+    print (confmc10, confmc11)
