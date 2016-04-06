dff646dcfff01122c9cf56f981417f13d1971069
lidaina22 <774832829@qq.com>
2016-04-07 00:44:04 +0800

    Added files via upload
    rename: arithmeticAsianOptionCalculator.html, add option price to result printing, delete % in html for interest rate

---
+++ index.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: Assignment3
+# File name: index
+# Author: Mark Wang
+# Date: 29/3/2016
+
+import web
+
+from __init__ import render
+from eu_black_scholes import EuropeanOptionHtml
+from arithmetic_mean_basket_options import ArithmeticMeanBasketOptionsHTML
+from geometricOptions import GeometricOptionHtml
+from Binomial import BinomialTreeHtml
+from impliedVol import ImpliedVolHtml
+from geometricOptions import  GeometricBasketHtml
+from src.arithmeticAsianOptionPricer import ArithmeticAsianOptionPricerHtml
+
+
+class Index(object):
+    def GET(self):
+        return '''<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index page of option calculator</title>
+</head>
+<body>
+<h1>Option Calculator</h1>
+<a href="/eu_bs">European call/put option Calculator (Using Black-Scholes)</a><br>
+<a href="/am_bo">Arithmetic/Geometric mean basket call/put options calculator</a><br>
+<a href="/eu_goa">Geometric Asian call/put options calculator</a><br>
+<a href="/eu_gob">Geometric Basket call/put options calculator</a><br>
+<a href="/eu_bt">American call/put option Calculator (Using Binomial Tree)</a><br>
+<a href="/im_vol">Implied Volatility</a><br>
+<a href="/aa_price">Arithmetic Asian Option Calculator (Monte Carlo)</a><br>
+</body>
+</html>'''
+
+urls = (
+    '/am_bo', ArithmeticMeanBasketOptionsHTML,
+    '/eu_bs', EuropeanOptionHtml,
+    '/eu_goa', GeometricOptionHtml,
+    '/eu_gob', GeometricBasketHtml,
+    '/eu_bt', BinomialTreeHtml,
+    '/im_vol', ImpliedVolHtml,
+    '/aa_price', ArithmeticAsianOptionPricerHtml,
+    '/', Index
+)
+app = web.application(urls, globals())
+
+
+if __name__ == "__main__":
+    app.run()
