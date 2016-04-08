bdc7db8c596c1cdbf8b90a1aec5c294b9c4f0d63
lidaina <774832829@qq.com>
2016-04-08 18:48:57 +0800

    Add files

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Project: Assignment3
+# File name: __init__.py
+# Author: Mark Wang
+# Date: 28/3/2016
+# init file
+
+import web
+
+render = web.template.render('.')
